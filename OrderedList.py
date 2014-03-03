7f48b8d0f3e68c2aa273c685582075961a8f65d2
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-03-03 07:22:31 -0800

    A better version of ordered list
    It still contains inconsistent in its function class

---
+++ OrderedList.py
@@ -0,0 +1,86 @@
+from bisect import bisect_left, bisect_right
+
+class Orderedlist:
+    def __init__(self,_ordered = []):
+        self.ordered = _ordered
+    
+    def indexAbove(self,x):
+        try:
+            i = bisect_left(self.ordered, x)
+            return i
+        except IndexError: # We've hit an element above everything
+            return -1
+
+    def indexBelow(self,x):
+        '''Returns the index of the largest or equal element below'''
+        try:
+            i = bisect_right(self.ordered, x)
+            return i-1
+        except IndexError: # We've hit an element above everything
+            return -1
+
+    def __contains__(self,x):
+        i = bisect_left(self.ordered, x)
+        if i != len(self.ordered) and self.ordered[i] == x:
+            return i
+        raise ValueError
+
+    def __getitem__(self,pos):
+        start, stop = pos.start,pos.stop
+        if start > stop: return ()
+        return self.ordered[self.indexAbove(start):self.indexBelow(stop)+1]
+    
+    def __str__(self):
+        return str(self.ordered)
+
+import unittest
+class TestOrdered(unittest.TestCase):
+    def test_ordered(self):
+        m = (-1,2,3,4,10,33,141)
+        l = Orderedlist(m)
+        self.assertEqual(m[l.indexBelow(2)],2)
+        self.assertEqual(m[l.indexBelow(15)], 10)
+
+        self.assertEqual(m[l.indexBelow(3)], 3)
+        self.assertEqual(m[l.indexBelow(2200)], 141)
+        self.assertEqual(m[l.indexAbove(4)], 4)
+        self.assertEqual(m[l.indexAbove(10)], 10)
+        self.assertEqual(m[l.indexAbove(1)], 2)
+        self.assertEqual(m[l.indexAbove(5)], 10)
+        self.assertEqual(m[l.indexAbove(4)], 4)
+        self.assertEqual(m[l.indexAbove(2)], 2)
+        self.assertEqual(m[l.indexAbove(140)], 141)
+        self.assertEqual(m[l.indexAbove(0)], 2)
+        self.assertEqual(m[l.indexAbove(-5)], -1)
+        self.assertEqual(m[l.indexAbove(3)], 3)
+        self.assertEqual(l[3:4], (3,4))
+        self.assertEqual(l[5:11], (10,))
+        self.assertEqual(l[4:11], (4,10))
+        self.assertEqual(l[1:11], (2,3,4,10))
+        self.assertEqual(l[11:1], ())
+        m = (2,3,5)
+        l = Orderedlist(m)
+        self.assertEqual(l[1:100], (2,3,5))
+        self.assertEqual(l[3:100], (3,5))
+
+    def test_badContainer(self):
+        m = (10,15)
+        l = Orderedlist(m)
+        self.assertEqual(l.indexAbove(1), 0)
+        self.assertEqual(l.indexAbove(10), 0) 
+        self.assertEqual(l.indexAbove(15), 1)
+        self.assertEqual(l.indexAbove(20), 2)
+
+    def test_containment(self):
+        from PE_primes import primesUpTo
+        m = Orderedlist(primesUpTo(1000))
+        self.assertTrue(101 in m)
+        self.assertRaises(ValueError, lambda x: x in m, 100)
+
+    def test_print(self):
+        m = (-1,2,3,4,10,33,141)
+        l = Orderedlist(m)
+        self.assertTrue(str(l), str(m))
+
+if __name__ == "__main__":
+    unittest.main()
--- PE_factors.py
+++ PE_factors.py
@@ -1,58 +1,4 @@
-class orderedlist:
-	def __init__(self,_ordered = []):
-		self.ordered = _ordered
-	
-	def indexAbove(self,q):
-		'''Returns the index of the largest or equal element above
-		in hopefully log(n) time, n = len(self.ordered)'''
-		# There is no elements greater than q in the list
-		# returns an error. Maybe the better choice is to return an -1
-		if q > self.ordered[-1]:
-			raise IndexError
-		# If it is lower than the entire list, return 0 (the rest of the algorithm breaks in any case)
-		if q < self.ordered[0]: return 0
-		# we now try to narrow down 
-		low, high = 0, len(self.ordered) - 1
-		while True:
-			guess = low + (high - low)//2
-			if self.ordered[guess] > q:
-				high = guess
-			else:
-				low = guess
-			# we now narrowed it enough that we only need to check one last element
-			if (high - low) <= 1:
-				if self.ordered[low] == q: return low
-				else: return high
 
-	def indexBelow(self,q):
-		'''Returns the index of the largest or equal element above
-		in hopefully log(n) time, n = len(self.ordered)'''
-		# There is no elements greater than q in the list
-		# returns an error. Maybe the better choice is to return an -1
-		if q < self.ordered[0]:
-			raise IndexError
-		# If it is lower than the entire list, return 0 (the rest of the algorithm breaks in any case)
-		if q > self.ordered[-1]: return len(self.ordered)-1
-		# we now try to narrow down 
-		low, high = 0, len(self.ordered) - 1
-		while True:
-			guess = low + (high - low)//2	
-			if self.ordered[guess] < q:
-				low = guess
-			else:
-				high = guess
-			# we now narrowed it enough that we only need to check one last element
-			if (high - low) <= 1:
-				if self.ordered[high] == q: return high
-				else: return low
-
-	def __getitem__(self,pos):
-		start, stop = pos.start,pos.stop
-		if start > stop: return ()
-		return self.ordered[self.indexAbove(start):self.indexBelow(stop)+1]
-	
-	def __str__(self):
-		return str(self.ordered)
 
 from operator import mul
 from functools import reduce
@@ -101,31 +47,5 @@ for a in genFactors(1000):
 	count += 1
 print(count)
 
-m = (-1,2,3,4,10,33,141)
-l = orderedlist(m)
-print(m[l.indexBelow(2)] == 2)
-print(m[l.indexBelow(15)] == 10)
-print(m[l.indexBelow(3)] == 3)
-print(m[l.indexBelow(3)])
-print(m[l.indexBelow(2200)] == 141)
-print(m[l.indexAbove(4)] == 4)
-print(m[l.indexAbove(10)] == 10)
-print(m[l.indexAbove(1)] == 2)
-print(m[l.indexAbove(5)] == 10)
-print(m[l.indexAbove(4)] == 4)
-print(m[l.indexAbove(2)] == 2)
-print(m[l.indexAbove(140)] == 141)
-print(m[l.indexAbove(0)] == 2)
-print(m[l.indexAbove(-5)] == -1)
-print(m[l.indexAbove(3)] == 3)
-print("Testing intervals")
-print(l[3:4] == (3,4))
-print(l[5:11] == (10,))
-print(l[4:11] == (4,10))
-print(l[1:11] == (2,3,4,10))
-print(l[11:1] == ())
-m = (2,3,5)
-l = orderedlist(m)
-print(l[1:100] == (2,3,5))
-print(l[3:100] == (3,5))
+
 '''
